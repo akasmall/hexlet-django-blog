6e16c26b60671fe19751cf7c24aac122aaffc7ac
akasmall <aka_sm@ro.ru>
2024-12-21 16:05:26 +0300

    completed lesson 6 modules 4

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -10,3 +10,9 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def base(request):
+    return render(request, 'base.html', context={
+        'who': 'World',
+    })
