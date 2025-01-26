512aecb33e693d0654b629b39252559fcfc06d9f
akasmall <aka_sm@ro.ru>
2025-01-26 20:28:22 +0300

    completed - development on the django - modules 4 - lesson 5 - templating language

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,10 @@
 # hexlet_django_blog/article/views.py
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('article')
+    return render(
+        request,
+        'article/index.html',
+        context={'article': 'article'},
+    )
